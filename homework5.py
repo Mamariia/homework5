c610cd42d6c745b598cac7da071e980443c567ca
Mamariia <148610516+Mamariia@users.noreply.github.com>
2024-04-30 15:20:04 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['apple', 'banana', 'orange', 'kiwi', 'peach']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:6])
+my_list[2] = 'pear'
+print(my_list)
+
+my_dict = {'Dog': 'Собака', 'Cat': 'Кошка', 'Mouse': 'Мышь'}
+print(my_dict)
+print(my_dict['Cat'])
+my_dict['Mouse'] = 'Мышка'
+print(my_dict)
